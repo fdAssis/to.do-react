80d4a6274fe3edb017d33a501c703d67beb315f8
fdAssis <franciscodeassisc.santos@gmail.com>
2021-10-08 18:03:10 -0300

    fuction handleTooggleTaskCompletion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -28,7 +28,13 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    tasks.map(task => {
+      if (task.id == id){
+        task.isComplete = !task.isComplete;
+        setTasks([...tasks])
+      }
+    });
   }
 
   function handleRemoveTask(id: number) {
